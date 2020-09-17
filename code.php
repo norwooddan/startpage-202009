030643f2cb67d66b5363afa92a4e137c4742bfe0
norwooddan <norwood.dan@gmail.com>
2020-09-17 01:58:09 -0700

    Update code.php
    Added $randomNumber. Creating a random number between 1-10000 and appends it to the weather icon url ($iconurl) so that it won't be cached.

--- code.php
+++ code.php
@@ -9,13 +9,12 @@ $record = $reader->city($_SERVER['REMOTE_ADDR']);
 $userCity = $record->city->name;
 
 // Openweathermap
-$jsonurl = "https://api.openweathermap.org/data/2.5/weather?q=" . $userCity . "&units=imperial&appid=[insert_token_here]";
+$jsonurl = "https://api.openweathermap.org/data/2.5/weather?q=" . $userCity . "&units=imperial&appid=05e8c7e2aa4b598ad62f33c75b2ea1c1";
 $json = file_get_contents($jsonurl);
 $weather = json_decode($json);
 $description = $weather->weather[0]->description;
 $description = ucfirst($description);
 $descriptionicon = $weather->weather[0]->icon;
-$iconurl = "https://openweathermap.org/img/wn/" . $descriptionicon . "@2x.png";
 $description2 = $weather->weather[1]->description;
 $descriptionicon2 = $weather->weather[1]->icon;
 $description2 = ucfirst($description2);
@@ -25,4 +24,6 @@ $tempnow = $weather->main->temp;
 $templow = $weather->main->temp_min;
 $temphigh = $weather->main->temp_max;
 $windspeed = $weather->wind->speed;
+$randomNumber = rand(1,10000);
+$iconurl = "https://openweathermap.org/img/wn/" . $descriptionicon . "@2x.png" . "?" . $randomNumber;
 ?>
